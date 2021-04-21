c8f49fd939320c54b8b3bf5aa664935a3b3cbaf8
Doug Gaff <doug.gaff@gmail.com>
2021-04-21 19:16:16 -0400

    updated to get the moon phase from internet

--- moonlight.ino
+++ moonlight.ino
@@ -1,6 +1,17 @@
+#include "NTPClient.h"
+#include "ESP8266WiFi.h"
+#include "WiFiUdp.h"
+#include "TimeLib.h"
+#include "GDBStub.h"
+
+#include "moonPhase.h"
+
+// Don't save your wifi to GitHub! Put these two declarations in your wifisecrets.h file and replace with your SSID and PW.
+// const char *ssid     = "SSID";
+// const char *password = "WIFI Password";
+#include "wifisecrets.h"
 
 // Wemos pin mapping
-
 // #define A0 not sure  ADC0
 #define D0 16 // WAKE
 #define D1 5  // SCL
@@ -14,11 +25,43 @@
 #define TX 1  // TX
 #define RX 3  // RX
 
-// Set all digital pins to output
+// Turn on serial output for debugging
+//#define SERIAL
 
+// Time info
+//const long utcOffsetInSeconds = -18000; // EST
+const long utcOffsetInSeconds = -14400; // EDT
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "north-america.pool.ntp.org", utcOffsetInSeconds);
+
+// For getting the moon phase
+moonPhase moonPhaseV;                       
+
+// VT100 Terminal Emulation
+#define OFF         "\033[0m"
+#define BOLD        "\033[1m"
+#define LOWINTENS   "\033[2m"
+#define UNDERLINE   "\033[4m"
+#define BLINK       "\033[5m"
+#define REVERSE     "\033[7m"
+#define GOTOXY( x,y) "\033[x;yH"   // Esc[Line;ColumnH  <-- this doesn't seem to be expanding correclty
+#define CLS          "\033[2J"
+
+// Run once
 void setup()
 {
-  pinMode(D0, OUTPUT); // Digital Output
+  // Serial port output. Note that GDB isn't working reliably yet.
+#ifdef SERIAL  
+  Serial.begin(115200);
+//  gdbstub_init();
+#endif
+
+  // Set all pins to digital output
+  pinMode(D0, OUTPUT); 
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
@@ -27,10 +70,18 @@ void setup()
   pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
   pinMode(D8, OUTPUT);
+
+  // Connect to Wifi
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+  }
+
+  // Initialize NTP time
+  timeClient.begin();
 }
 
-// Sets the phase for each segment
-
+// Lights up different segments of the moon light.
 void setPhase(int s1, int s2, int s3, int s4, int s5, int s6)
 {
   if (s1) digitalWrite(D7, HIGH);   
@@ -52,45 +103,130 @@ void setPhase(int s1, int s2, int s3, int s4, int s5, int s6)
   else digitalWrite(D1, LOW);    
 }
 
-// Loop through the phases
-
-void loop()
+// Test function to run through all phases.
+void cyclePhases()
 {
-  setPhase(1, 0, 0, 0, 0, 0);  
+  setPhase(0, 0, 0, 0, 0, 0);  
+  delay(500);    
+  
+  setPhase(0, 0, 0, 0, 0, 1);  
   delay(500);              
   
-  setPhase(1, 1, 0, 0, 0, 0);  
+  setPhase(0, 0, 0, 0, 1, 1);  
   delay(500);              
-  
-  setPhase(1, 1, 1, 0, 0, 0);  
-  delay(500);              
-  
-  setPhase(1, 1, 1, 1, 0, 0);  
-  delay(500);              
-  
-  setPhase(1, 1, 1, 1, 1, 0);  
-  delay(500);              
-  
-  setPhase(1, 1, 1, 1, 1, 1);  
-  delay(500);              
-  
-  setPhase(0, 1, 1, 1, 1, 1);  
+
+  setPhase(0, 0, 0, 1, 1, 1);  
   delay(500);              
   
   setPhase(0, 0, 1, 1, 1, 1);  
   delay(500);              
   
-  setPhase(0, 0, 0, 1, 1, 1);  
+  setPhase(0, 1, 1, 1, 1, 1);  
   delay(500);              
   
-  setPhase(0, 0, 0, 0, 1, 1);  
+  setPhase(1, 1, 1, 1, 1, 1);  
   delay(500);              
   
-  setPhase(0, 0, 0, 0, 0, 1);  
+  setPhase(1, 1, 1, 1, 1, 0);  
   delay(500);              
   
-  setPhase(0, 0, 0, 0, 0, 0);  
+  setPhase(1, 1, 1, 1, 0, 0);  
+  delay(500);              
+  
+  setPhase(1, 1, 1, 0, 0, 0);  
   delay(500);              
 
-//  yield();
+  setPhase(1, 1, 0, 0, 0, 0);  
+  delay(500);              
+  
+  setPhase(1, 0, 0, 0, 0, 0);  
+  delay(500);               
+}
+
+// Set approximate phase angle.
+// Credit: https://blogs.jccc.edu/astronomy/lab-manual/experiment-two-lunar-phases/
+void setMoonPhase(int angle)
+{
+  if (angle >= 0 && angle < 15)
+    setPhase(0, 0, 0, 0, 0, 0); // New Moon
+  else if (angle >= 15 && angle < 45)
+    setPhase(0, 0, 0, 0, 0, 1); // Waxing Crescent
+  else if (angle >= 45 && angle < 75)
+    setPhase(0, 0, 0, 0, 1, 1); // Waxing Crescent
+  else if (angle >= 75 && angle < 105)
+    setPhase(0, 0, 0, 1, 1, 1); // 1st Quarter (half moon)
+  else if (angle >= 105 && angle < 135)
+    setPhase(0, 0, 1, 1, 1, 1); // Waxing Gibbous
+  else if (angle >= 135 && angle < 165)
+    setPhase(0, 1, 1, 1, 1, 1); // Waxing Gibbous
+  else if (angle >= 165 && angle < 195)
+    setPhase(1, 1, 1, 1, 1, 1); // Full Moon
+  else if (angle >= 195 && angle < 225)
+    setPhase(1, 1, 1, 1, 1, 0); // Waning Gibbous
+  else if (angle >= 225 && angle < 255)
+    setPhase(1, 1, 1, 1, 0, 0); // Waning Gibbous
+  else if (angle >= 255 && angle < 285)
+    setPhase(1, 1, 1, 0, 0, 0); // 3rd Quarter (half moon)
+  else if (angle >= 285 && angle < 315)
+    setPhase(1, 1, 0, 0, 0, 0); // Waning Crescent
+  else if (angle >= 315 && angle < 345)
+    setPhase(1, 0, 0, 0, 0, 0); // Waning Crescent
+  else // 345 to 360
+    setPhase(0, 0, 0, 0, 0, 0); // New Moon
+}
+
+// Main Loop
+
+int startupTestCounter = 0;
+
+void loop()
+{
+  // Cycle the phases on power up a couple of times
+  if (startupTestCounter < 2)
+  {
+    cyclePhases();
+    startupTestCounter++;
+  }
+
+  // Read the time
+  timeClient.update();
+  time_t time = timeClient.getEpochTime();
+
+  // Calculate the moon phase
+  moonData_t moon;              
+  moon = moonPhaseV.getPhase(time-utcOffsetInSeconds);
+
+  // We don't need to set the phase very often, since it doesn't change much. Set it once after the startup sequence, then
+  // check the phase every hour and set it.
+  if (startupTestCounter == 2) setMoonPhase(moon.angle);
+  else if (time % 3600 == 0) setMoonPhase(moon.angle);
+
+#ifdef SERIAL
+  char timestr[10];
+  Serial.print("\033[0;0H"); // terminal emulation to go to top
+  sprintf(timestr, "%02d:%02d:%02d", timeClient.getHours(), timeClient.getMinutes(), timeClient.getSeconds());
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(day(time));
+  Serial.print(" ");
+  Serial.print(month(time));
+  Serial.print(" ");
+  Serial.print(year(time));
+  Serial.print(" ");
+  Serial.print(", ");
+  Serial.print(timestr);
+  Serial.println("");
+  //Serial.println(timeClient.getFormattedTime());
+
+  // Print moon info
+  Serial.print( "Moon phase angle: " );
+  Serial.print( moon.angle );             // angle is a integer between 0-360
+  Serial.println( " degrees." );
+  Serial.print( "Moon surface lit: " );
+  Serial.print( moon.percentLit * 100 );  // percentLit is a real between 0-1
+  Serial.println( " %" );
+#endif  
+
+//  delay(500);          
+  yield();
 }
